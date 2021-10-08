ad7dc55e94e1498f62c47bea911e97c4a66292f7
soottoppi <duwjs1103@naver.com>
2021-10-08 16:59:36 +0900

    emaillist02 #2

--- emaillist02/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist02/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -19,11 +19,31 @@ public class EmaillistController extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		
 		String action = request.getParameter("a");
-		if("list".equals(action)) {
+		if("form".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);
+			
+		} else if("add".equals(action)){
+			request.setCharacterEncoding("utf-8");
+
+			String firstName = request.getParameter("fn");
+			String lastName = request.getParameter("ln");
+			String email = request.getParameter("email");
+			
+			EmaillistVo vo = new EmaillistVo();
+			vo.setFirstName(firstName);
+			vo.setLastName(lastName);
+			vo.setEmail(email);
+			
+			new EmaillistDao().insert(vo);
+			
+			response.sendRedirect("/emaillist02/el");
+		} else {
 			EmaillistDao dao = new EmaillistDao();
 			List<EmaillistVo> list = dao.findAll();
 			
 			request.setAttribute("list", list);
+			
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
 			rd.forward(request, response);
 		}
--- emaillist02/src/main/webapp/WEB-INF/views/form.jsp
+++ emaillist02/src/main/webapp/WEB-INF/views/form.jsp
@@ -11,7 +11,8 @@
 		메일 리스트에 가입하려면,<br>
 		아래 항목을 기입하고 등록 버튼을 클릭하세요.
 	</p>
-	<form action="add.jsp" method="post">
+	<form action="/emaillist02/el" method="post">
+		<input type='hidden' name="a" value="add">
 	    First name: <input type="text" name="fn" value="" ><br>
 	    Last name: <input type="text" name="ln" value=""><br>
 	    Email address: <input type="text" name="email" value=""><br>
@@ -19,7 +20,7 @@
 	</form>
 	<br>
 	<p>
-		<a href="/emaillist01">리스트 바로가기</a>
+		<a href="/emaillist02/el">리스트 바로가기</a>
 	</p>
 </body>
 </html>
